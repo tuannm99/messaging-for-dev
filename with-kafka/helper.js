995c8a456e6513ec7aef2a6406d70e3fb4544d40
tuannm99 <tuannm@foobla.com>
2024-04-30 01:30:10 +0700

    init kafka

--- with-kafka/helper.js
+++ with-kafka/helper.js
@@ -2,11 +2,12 @@ const kafka = require('node-rdkafka')
 const {KAFKA_BROKER_LIST} = require('./constants')
 
 /**
- * @returns {kafka.Producer} producer
+ * @returns {Promise<kafka.Producer>} producer
  */
 const kafkaProducer = async () => {
     const producer = new kafka.Producer({
         'metadata.broker.list': KAFKA_BROKER_LIST,
+        // 'allow.auto.create.topics': true,
         'client.id': 'kafka_client_id_1',
         'compression.codec': 'gzip',
         'retry.backoff.ms': 200,
@@ -18,10 +19,12 @@ const kafkaProducer = async () => {
         dr_cb: true
     })
 
-    producer.on('connection.failure', (error, _clientMetrics) => {
-        console.log('Connect failed', error)
-        // console.log('❤❤❤ tuannm: [simple-pubsub.js][11][error]', error)
-        // console.log('❤❤❤ tuannm: [simple-pubsub.js][11][clientMetrics]', clientMetrics)
+    producer.on('connection.failure', (error, clientMetrics) => {
+        if (error) {
+            console.log('Connect failed', error)
+            return
+        }
+        console.log('clientMetrics', clientMetrics)
     })
 
     // Any errors we encounter, including connection errors
@@ -51,7 +54,85 @@ const kafkaProducer = async () => {
         .catch((err) => console.error(err))
 }
 
-const kafkaConsumer = async () => {}
+/**
+ * @returns {Promise<kafka.KafkaConsumer>} consumer
+ */
+const kafkaConsumer = async () => {
+    const consumer = new kafka.KafkaConsumer(
+        {
+            'metadata.broker.list': KAFKA_BROKER_LIST,
+            'group.id': 'kafka',
+            'partition.assignment.strategy': 'roundrobin',
+            // 'enable.auto.commit': true
+            rebalance_cb: (err, assignment) => {
+                if (err) {
+                    if (err.code === kafka.CODES.ERRORS.ERR__ASSIGN_PARTITIONS) {
+                        console.error('❤❤❤ tuannm: [helper.js][66][err]', err)
+                        // TODO: -> reassign
+                    } else if (err.code == kafka.CODES.ERRORS.ERR__REVOKE_PARTITIONS) {
+                        console.error('❤❤❤ tuannm: [helper.js][69][err]', err)
+                        // TODO: -> reassign
+                    } else {
+                        console.error(err)
+                    }
+                    return null
+                }
+                console.log('❤❤❤ tuannm: [helper.js][61][assignment]', assignment)
+            },
+            offset_commit_cb: (err, topicPartitions) => {
+                if (err) {
+                    // There was an error committing
+                    console.error(err)
+                    return
+                } else {
+                    // Commit went through. Let's log the topic partitions
+                    console.log(topicPartitions)
+                }
+            }
+        },
+        {}
+    )
+
+    consumer.on('connection.failure', (error, _clientMetrics) => {
+        console.log('Connect failed', error)
+    })
+
+    consumer.on('event.error', (err) => {
+        console.error('Error from producer', err)
+    })
+
+    consumer.on('ready', (info, _metadata) => {
+        console.log(`ready info=${JSON.stringify(info)}`)
+    })
+
+    consumer.on('rebalance', (err, assignments) => {
+        if (err) {
+            console.error(`rebalance error=${JSON.stringify(err)}`)
+            return
+        }
+        console.log(`assignments: ${JSON.stringify(assignments)}`)
+    })
+
+    consumer.on('offset.commit', (err, topicPartitions) => {
+        if (err) {
+            console.error(`offset commit error=${JSON.stringify(err)}`)
+            return
+        }
+        console.log(`topic pattitions: ${JSON.stringify(topicPartitions)}`)
+    })
+
+    return new Promise((resolve, reject) =>
+        consumer.connect({}, (e, _data) => {
+            if (e) reject(e)
+
+            resolve()
+        })
+    )
+        .then(() => {
+            return consumer
+        })
+        .catch((err) => console.error(err))
+}
 
 const kafkaAdmin = async () => {}
 
--- with-kafka/kafka/simple-pubsub.js
+++ with-kafka/kafka/simple-pubsub.js
@@ -1,11 +1,111 @@
-const kafka = require('node-rdkafka')
-const {kafkaProducer} = require('../helper')
+const Kafka = require('node-rdkafka')
+const {KAFKA_BROKER_LIST} = require('../constants')
+const Bluebird = require('bluebird')
 
-;(async () => {
-    const producer = await kafkaProducer()
+// after reading `node-rdkafka` modules, I found myself was a fool.
+// The lib itself always singleton, we can alway call new and pass configuration so don't tryna make it singleton:))
+function createConsumer(config, onData) {
+    const consumer = new Kafka.KafkaConsumer(config, {'auto.offset.reset': 'earliest'})
 
-    producer.produce('topic-1', null, Buffer.from('hello world4'))
-    producer.flush(5000)
+    return new Promise((resolve, reject) => {
+        consumer
+            .on('ready', () => {
+                console.log('consumer ready')
+                resolve(consumer)
+            })
+            .on('data', onData)
+            .on('connection.failure', (err) => reject(err))
 
-    process.exit(0)
-})()
+        consumer.connect()
+    })
+}
+
+async function consume() {
+    const config = {
+        'metadata.broker.list': KAFKA_BROKER_LIST,
+        'group.id': 'kafka_client_id_1'
+    }
+
+    //let seen = 0;
+    let topic = 'purchases'
+
+    const consumer = await createConsumer(config, ({key, value}) => {
+        let k = key.toString().padEnd(10, ' ')
+        console.log(`Consumed event from topic ${topic}: key = ${k} value = ${value}`)
+    })
+
+    consumer.subscribe([topic])
+    consumer.consume()
+
+    process.on('SIGINT', () => {
+        console.log('\nDisconnecting consumer ...')
+        consumer.disconnect()
+    })
+}
+
+function createProducer(config, onDeliveryReport) {
+    const producer = new Kafka.Producer(config)
+
+    return new Promise((resolve, reject) => {
+        producer
+            .on('ready', () => resolve(producer))
+            .on('delivery-report', onDeliveryReport)
+            .on('event.error', (err) => {
+                console.warn('event.error', err)
+                reject(err)
+            })
+        producer.connect()
+    })
+}
+
+async function produce() {
+    const config = {
+        'metadata.broker.list': KAFKA_BROKER_LIST,
+        'group.id': 'group-1',
+        dr_msg_cb: true
+    }
+
+    let topic = 'purchases'
+
+    let users = ['eabara', 'jsmith', 'sgarcia', 'jbernard', 'htanaka', 'awalther']
+    let items = ['book', 'alarm clock', 't-shirts', 'gift card', 'batteries']
+
+    const producer = await createProducer(config, (err, report) => {
+        if (err) {
+            console.warn('Error producing', err)
+        } else {
+            const {topic, key, value} = report
+            let k = key.toString().padEnd(10, ' ')
+            console.log(`Produced event to topic ${topic}: key = ${k} value = ${value}`)
+        }
+    })
+
+    let numEvents = 10
+    for (let idx = 0; idx < numEvents; ++idx) {
+        const key = users[Math.floor(Math.random() * users.length)]
+        const value = Buffer.from(items[Math.floor(Math.random() * items.length)])
+
+        producer.produce(topic, -1, value, key)
+    }
+
+    producer.flush(10000, () => {
+        producer.disconnect()
+    })
+}
+
+consume()
+    .then(async () => {
+        produce()
+            .then()
+            .catch((err) => {
+                console.error(`Something went wrong:\n${err}`)
+                process.exit(1)
+            })
+
+        await Bluebird.delay(5000)
+        process.exit(1)
+    })
+    .catch((err) => {
+        console.error(`Something went wrong:\n${err}`)
+        process.exit(1)
+    })
