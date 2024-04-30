6af70b8d6bc6e3fa64a192cd2bf0b5fbb1f333f5
tuannm99 <tuannm@foobla.com>
2024-05-01 00:30:02 +0700

    update

--- with-rabbit/plugins/delay-queue/index.js
+++ with-rabbit/plugins/delay-queue/index.js
@@ -1,6 +1,6 @@
 // NO CODE Explain
-// delay queue means message go to an exchange push to the queue after a initial time
+// delay queue means message go to an special exchange, push to the queue after a initial time
+//
 // 1. it not built-in, first we need to enable plugin => rabbitmq-plugins enable rabbitmq_delayed_message_exchange
-
 // 2. create an exchange with extra args:  {"x-delayed-type": "direct"}
 // 3. publish with headers: {"x-delay": 5000}
