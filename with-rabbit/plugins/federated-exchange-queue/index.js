4b69f63ae449ec68ffe9aabcda5fac3bf2271702
tuannm99 <tuannm@foobla.com>
2024-05-04 00:08:24 +0700

    update

---
+++ with-rabbit/plugins/federated-exchange-queue/index.js
@@ -0,0 +1,3 @@
+// Federation plugin is to transmit messages between brokers without requiring clustering
+// ==> enable rabbitmq_federation plugin
+// this is a advance features which I never done before, so checkout documentation => https://www.rabbitmq.com/docs/federation
